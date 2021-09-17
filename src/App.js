bc256bc4cbc3211db64de6a3830d8614914a21d4
cbuiles <cgomezb@unal.edu.co>
2021-09-16 22:24:40 -0500

    Clase 32 Crud API Creando una API con JSON Server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import CrudApp from "./components/CrudApp";
+import CrudApi from "./components/CrudApi";
 
 function App() {
   return (
     <>
       <h1>Ejercicios con React</h1>
+      <CrudApi />
+      <hr />
       <CrudApp />
       <hr />
     </>
