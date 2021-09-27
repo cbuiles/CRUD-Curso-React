ce8d306be466fe961df33ab32d6f1b5b1b9bcdba
cbuiles <cgomezb@unal.edu.co>
2021-09-26 20:20:15 -0500

    Clase 43. Selects Anidados:  Hook personalizado para peticiones Fetch(2/3)

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,54 @@
+// Los HOoks personalizados se caracterizan por devolver logica y no hacer tan robusto el componente principal donde se va utilizar
+
+// Hoook personalizado para cuando necesitemso hacer una peticion Fetch y necesitamos guardarlo en variables de estado
+import { useState, useEffect } from "react";
+
+export const useFectch = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+          let err = new Error("Error en la petición Fetch");
+          err.status = res.status || "00";
+          err.statusText = res.statusText || "Ocurrió un error";
+
+          throw err;
+        }
+
+        const json = await res.json();
+
+        // En caso de que no se haya ejecutado el AbortController
+        if (!signal.aborted) {
+          setData(json);
+          setError(null);
+        }
+      } catch (error) {
+        if (!signal.aborted) {
+          setData(null);
+          setError(error);
+        }
+      } finally {
+        if (!signal.aborted) {
+          setLoading(false);
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => abortController.abort();
+  }, [url]);
+
+  return { data, error, loading };
+};
